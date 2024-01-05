d3751e361ebd36644f08dfa9490634ca0ab86329
banjtheman <banjtheman@gmail.com>
2024-01-05 00:22:56 -0500

    update from global

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="localrag",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "unstructured",
